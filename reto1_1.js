99f88c5cde1b27d86d40155db33a82c66b16fe58
Reynaldo810 <66183593+Reynaldo810@users.noreply.github.com>
2020-06-12 22:31:27 -0500

    Create reto1_1.js

---
+++ reto1_1.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400); 
+  background(0);
+  stroke(250);
+  line(0,400,200,200,0);
+  line(400,400,200,200);
+  line(200,200,200,0);
+}
+
+function draw() {
+  background()
+}
