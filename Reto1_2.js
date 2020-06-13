45cf046fcf8b6d87a1f7fa1c644c889537b665fa
Reynaldo810 <66183593+Reynaldo810@users.noreply.github.com>
2020-06-12 22:32:15 -0500

    Create Reto1_2.js

---
+++ Reto1_2.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(220);
+  fill(255,0,0);
+  rect(0,0,200,600);
+  fill(0,255,0);
+  rect(200,0,200,600);
+  fill(0,0,255);
+  rect(400,0,200,600);
+}
