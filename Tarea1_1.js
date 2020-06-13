286f694f695cfdec94b3362fe73b63dc739809d1
Reynaldo810 <66183593+Reynaldo810@users.noreply.github.com>
2020-06-12 22:28:31 -0500

    Create Tarea1_1.js

---
+++ Tarea1_1.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(250);
+  fill(0);
+  rect(100,0,100,100);
+  fill(0);
+  rect(300,0,100,100);
+  fill(0);
+  rect(0,100,100,100);
+  fill(0);
+  rect(200,100,100,100);
+  fill(0);
+  rect(100,200,100,100);
+  fill(0);
+  rect(300,200,100,100);
+  fill(0);
+  rect(0,300,100,100);
+  fill(0);
+  rect(200,300,100,100);
+}
